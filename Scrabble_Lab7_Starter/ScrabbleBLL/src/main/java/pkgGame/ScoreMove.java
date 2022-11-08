86aa21effaaa7e27d21650b72433472c03324f8e
BryantFerguson <113267180+BryantFerguson@users.noreply.github.com>
2022-11-07 22:23:04 -0500

    Add files via upload

---
+++ Scrabble_Lab7_Starter/ScrabbleBLL/src/main/java/pkgGame/ScoreMove.java
@@ -0,0 +1,51 @@
+package pkgGame;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import eNum.eMoveResult;
+
+public class ScoreMove {
+
+	private UUID ScoreMoveID;
+	private ArrayList<ScoreWord> arrScoreWord = new ArrayList<ScoreWord>();
+
+	public ScoreMove() {
+		super();
+		ScoreMoveID = UUID.randomUUID();
+	}
+
+	public UUID getScoreMoveID() {
+		return ScoreMoveID;
+	}
+
+	public void AddScoreWord(ScoreWord sw) {
+		arrScoreWord.add(sw);
+	}
+
+	public ArrayList<ScoreWord> getArrScoreWord() {
+		return arrScoreWord;
+	}
+
+	public int CalculateScoreMove() {
+		//TODO: Calculate Score Move
+		int sum=0;
+		for(ScoreWord sw: arrScoreWord) {
+			sum += sw.getScore();
+		}
+		return sum;
+	}
+	
+	public eMoveResult findMoveResult()
+	{
+		for (ScoreWord SM: arrScoreWord)
+		{
+			if (SM.getMoveResult() != eMoveResult.GoodMove)
+			{
+				return SM.getMoveResult();
+			}
+		}
+		return eMoveResult.GoodMove;
+	}
+
+}
